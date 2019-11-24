8c30179f45954a4abc4d2e891c5f6deb39a473d6
kotori <zouguanhan@gmail.com>
2019-11-24 12:27:49 -0800

    11-24-2019

--- src/main.js
+++ src/main.js
@@ -14,51 +14,82 @@ const {
     RaffleController, GuardController } = require('./danmu/controller.js');
 const RoomidHandler = require('./handler/roomidhandler.js');    // 弹幕监听播报高能房间号
 const RaffleHandler = require('./handler/rafflehandler.js');    // 高能监听播报抽奖数据
+const settings = require('./settings.json');
 const config = require('./global/config.js');
 const cprint = require('./util/printer.js');
 const Server = require('./server/host.js');
 
-const raise_nofile_limit = () => {
-    let limit = null;
+(function() {
 
-    try {
+    (function main() {
+        cprint('bilibili-monitor[1.0.0] successfully launched', colors.green);
 
-        // 提升Linux系统nofile上限 (连接数量)     
-        // 系统层面调整上限(Linux): /etc/security/limits.conf
-        if (process.platform === 'linux') {
-            const posix = require('posix');
-            const hard_limit = posix.getrlimit('nofile')['hard'];
-            limit = hard_limit;
-            posix.setrlimit('nofile', { 'soft': hard_limit });
-            cprint(`Unix nofile 上限调整至极限: ${hard_limit}`, colors.green);
+        read_args();
+        let limit = raise_nofile_limit();
+
+        let raffleHandler = new RaffleHandler();
+        let roomidHandler = new RoomidHandler();
+        let guardController = new GuardController(limit);
+        let raffleController = new RaffleController();
+        let server = new Server();
+
+
+        server.run();
+        guardController.run();
+        raffleHandler.run();
+        roomidHandler.run();
+    })();
+
+
+    function raise_nofile_limit() {
+        let limit = null;
+
+        try {
+
+            // 提升Linux系统nofile上限 (连接数量)     
+            // 系统层面调整上限(Linux): /etc/security/limits.conf
+            if (process.platform === 'linux') {
+                const posix = require('posix');
+                const hard_limit = posix.getrlimit('nofile')['hard'];
+                limit = hard_limit;
+                posix.setrlimit('nofile', { 'soft': hard_limit });
+                cprint(`Unix nofile 上限调整至极限: ${hard_limit}`, colors.green);
+            }
+
+        } catch (error) {}
+
+        return limit;
+    };
+
+    function read_args() {
+        if (process.argv.includes('-v')) {
+            config.verbose = true;
         }
-    
-    } catch (error) {}
+        if (process.argv.includes('--debug')) {
+            config.debug = true;
+        }
 
-    return limit;
-};
+        config.ip = settings['server']['ip'];
+        config.port = settings['server']['port'];
 
+        const ipIndex = process.argv.indexOf('--ip');
+        if (ipIndex !== -1) {
+            if (ipIndex + 1 < process.argv[ipIndex+1]) {
+                const ip = process.argv[ipIndex+1];
+                config.server['ip'] = ip;
+            }
+        }
 
-(function main() {
-    let limit = raise_nofile_limit();
-
-    let raffleHandler = new RaffleHandler();
-    let roomidHandler = new RoomidHandler();
-    let guardController = new GuardController(limit);
-    let raffleController = new RaffleController();
-    let server = new Server();
-
-    if (process.argv.includes('-v')) {
-        config.verbose = true;
+        const portIndex = process.argv.indexOf('--port');
+        if (portIndex !== -1) {
+            if (portIndex + 1 < process.argv[portIndex+1]) {
+                const port = Number.parseInt(process.argv[portIndex+1]);
+                if (!isNaN(port)) {
+                    config.server['port'] = port;
+                }
+            }
+        }
     }
-    if (process.argv.includes('--debug')) {
-        config.debug = true;
-    }
-
-    cprint('bilibili-monitor[1.0.0] successfully launched', colors.green);
-
-    server.run();
-    guardController.run();
-    raffleHandler.run();
-    roomidHandler.run();
 })();
+
+
